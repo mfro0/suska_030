1dce682793a73756871255346bf71d68c20f96ff
Markus Fröschle <mfro@mubf.de>
2021-11-22 16:55:04 +0100

    make char output (somehow) work in simulation

--- ghdl/tb.vhd
+++ ghdl/tb.vhd
@@ -11,6 +11,9 @@ end entity tb;
 architecture sim of tb is
     signal clk_25               : std_ulogic := '1';
 
+    --
+    -- convert a byte-organised array into a longword-organised
+    -- one.
     function to32(a : work.m68k_rom.ubyte_array) return memarray is
         variable res : memarray(0 to (a'length + 4) / 4)(31 downto 0);
         variable last32 : std_ulogic_vector(31 downto 0);
@@ -18,6 +21,7 @@ architecture sim of tb is
         for i in 0 to res'length - 3 loop
             res(i) := a(4 * i + 0) & a(4 * i + 1) & a(4 * i + 2) & a(4 * i + 3);
         end loop;
+
         last32 := (others => '0');
         for i in a'length mod 4 to 3 loop
             last32 := last32 or std_ulogic_vector(resize(shift_left(unsigned(a(i * 8)), i mod 4), 32));
@@ -28,7 +32,8 @@ architecture sim of tb is
         return res;
     end function to32;
 
-    signal memory               : memarray(0 to 100)(31 downto 0) := 
+    -- 400 bytes of 32 bit memory
+    signal memory               : memarray(0 to 100)(31 downto 0) :=
     (
         0 to (work.m68k_rom.m68k_binary'length + 3) / 4 => to32(work.m68k_rom.m68k_binary),
         others => std_ulogic_vector(unsigned'(x"4e714e71"))
@@ -65,11 +70,19 @@ architecture sim of tb is
     signal dsack_n              : std_ulogic_vector(1 downto 0);
     signal ipend_n              : std_ulogic;
     signal size_n               : std_ulogic_vector(1 downto 0);
-        
+
 begin
 
     reset_n <= '0', '1' after 520 * 40 ns;
     halt_n <= '0', '1' after 520 * 40 ns;
+
+    p_once : process
+    begin
+        for i in 0 to memory'length - 1 loop
+            report "memory(" & integer'image(i) & ") = " & to_hstring(memory(i)) severity note;
+        end loop;
+        wait;
+    end process p_once;
 
     p_clkit : process(all)
     begin
@@ -78,18 +91,39 @@ begin
 
     p_read : process(all)
         variable adr : integer;
+        variable cs  : string(1 to 1);
     begin
-        dsack_n <= (others => 'Z');
-        adr := to_integer(unsigned(adr_out)) / 4;
-        
-        if rising_edge(clk_25) then
-            if rw_n = '1' then
-                if adr >= memory'low and adr <= memory'high then
-                    data_in <= memory(adr);
+        --if rising_edge(clk_25) then
+            dsack_n <= (others => 'Z');
+
+            if as_n = '0' then
+                if adr_out = x"fffffff0" then
+                    if rw_n = '1' then      -- read cycle
+                        -- UART status register, ready when = '1'
+                        -- pretend we are always ready in simulation
+                        data_in <= x"00000001";
+                        dsack_n <= (others => '0');
+                    end if;
+                elsif adr_out = x"fffffff4" then
+                    -- UART output register
+                    if rw_n = '0' then                      -- write cycle
+                        report "value=" & to_hstring(data_out) severity note;
+                        cs(1) := character'val(to_integer(unsigned(data_out(7 downto 0))));
+                        dsack_n <= (others => '0');
+                        report cs severity note;
+                    end if;
+                elsif unsigned(adr_out) / 4 >= to_unsigned(memory'low, 32) and unsigned(adr_out) / 4 <= to_unsigned(memory'high, 32) then
+                    adr := to_integer(unsigned(adr_out)) / 4;
+
+                    if rw_n = '1' then
+                        data_in <= memory(adr);
+                    else        -- write to memory
+                        memory(adr) <= data_out;
+                    end if;
                     dsack_n <= (others => '0');           -- indicate 32 bit port size
                 end if;
             end if;
-        end if;
+        --end if;
     end process p_read;
 
     i_m68030 : entity work.wf68k30l_top
--- m68k/puts.c
+++ m68k/puts.c
@@ -1,7 +1,8 @@
-static inline int putc(int c)
+static int putchar(int c)
 {
     volatile int *term_ready = (volatile int *) 0xfffffff0;
     volatile int *term_out = (volatile int *) 0xfffffff4;
+
 
     while (!*term_ready);
     *term_out = c;
@@ -14,7 +15,7 @@ int puts(char *str)
     int c;
 
     while ((c = *str++))
-        putc(c);
+        putchar(c);
 
     return 0;
 }
