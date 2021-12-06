98b3607a15bdbf707f7eeb799458edfa3e4b5d66
Markus Fröschle <mfro@mubf.de>
2021-12-06 06:41:57 +0100

    doesn't work because of UB which is fully utilised with -O3

--- m68k/simple.c
+++ m68k/simple.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
-static short xx;
 int main(int argc, char *argv[])
 {
-    int x[] = { 0x01234567, 0x89abcdef };
+    int x[] = { 0, 0, 0 };
 
-    printf("simple test program\r\n");
+    printf("simple test\r\n");
 
-    for (int i = 0; i < sizeof(x); i++)
+    for (int i = 0; i < sizeof(x) - sizeof(long); i++)
     {
-        int y[sizeof(x)] = {x[0], x[1]};
+        int y[sizeof(x)] = {x[0], x[1], x[2]};
 
         int *unal = (int *) ((char *) &y + i);
-        *unal = 0xaffebeef;
+        *unal = 0x12345678;
 
         printf("y[] = %08x %08x\r\n", y[0], y[1]);
     }
 
     puts("");
 
-    for (int i = 0; i < sizeof(x); i++)
+    for (int i = 0; i < sizeof(x) - sizeof(long); i++)
     {
-        int y[sizeof(x)] = {x[0], x[1]};
+        int y[sizeof(x)] = {x[0], x[1], x[2]};
 
         short *unal = (short *) ((char *) &y + i);
-        *unal = 0xaffe;
+        *unal = 0x1234;
 
         printf("y[] = %08x %08x\r\n", y[0], y[1]);
     }
@@ -32,15 +31,16 @@ int main(int argc, char *argv[])
 
     puts("");
 
-    for (int i = 0; i < sizeof(x); i++)
+    for (int i = 0; i < sizeof(x) - sizeof(long); i++)
     {
-        int y[sizeof(x)] = {x[0], x[1]};
+        int y[sizeof(x)] = {x[0], x[1], x[2]};
 
         char *unal = (char *) ((char *) &y + i);
-        *unal = 0xaf;
+        *unal = 0x12;
 
         printf("y[] = %08x %08x\r\n", y[0], y[1]);
     }
+
     (void) * (volatile char *) 0xffffffff; /* stop program */
     return 0;
 }
